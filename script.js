c07fd8fccfb5fd86dda89a23461164899ce1d003
Pg45-brabo <bemestarstore7@gmail.com>
2026-04-03 14:46:09 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// style.css e script.js foram mantidos para compatibilidade legacy.
+// O projeto R&S Makeup agora usa store.css como folha de estilos principal.
+// Este arquivo pode ser ignorado.
